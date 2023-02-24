eded05af2dca85d8014facbd412714af31c0c264
StarrkF <faymergen98@gmail.com>
2023-02-25 00:53:51 +0300

    added blade component (alert & breadcrumb)

--- resources/views/Admin/includes/messages.blade.php
+++ resources/views/Admin/includes/messages.blade.php
@@ -1,25 +1,20 @@
 <!-- Validator Errors -->
 
 @if ($errors->any())
-<div class="alert alert-danger alert-dismissible show fade">
     @foreach ($errors->all() as $error)
-    <li>{{ $error }}</li>
+        <x-alert type="success" :message="$error" />
     @endforeach
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
 @endif
 
 
 @if (Session::has('success'))
-<div class="alert alert-success alert-dismissible show fade">
-    {{ session('success') }}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
+    <x-alert type="success" :message="session('success')" />
 @endif
 
 @if (Session::has('error'))
-<div class="alert alert-danger alert-dismissible show fade">
-    {{ session('error') }}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
+    <x-alert type="danger" :message="session('error')" />
+@endif
+
+@if (Session::has('info'))
+    <x-alert type="info" :message="session('info')" />
 @endif
