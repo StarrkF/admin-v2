8eb51cd74640a97ae6714351b900f055b0ed2915
StarrkF <faymergen98@gmail.com>
2023-03-09 18:32:25 +0300

    roles permission page

---
+++ app/Traits/ResponseTrait.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait ResponseTrait
+{
+    public function successResponse($data, $statusCode = 200): JsonResponse
+    {
+        $response = [
+            'message' => 'success',
+            'data' => $data
+        ];
+
+        return new JsonResponse($response, $statusCode);
+    }
+
+    public function errorResponse($errors, $message = 'Server Error', $statusCode = 500): JsonResponse
+    {
+        $data = [
+            'message' => $message,
+            'errors' => $errors,
+        ];
+
+        return new JsonResponse($data, $statusCode);
+    }
+
+    public function createdResponse($data): JsonResponse
+    {
+        $response = [
+            'message' => 'success',
+            'data' => $data
+        ];
+        return new JsonResponse($response, 201);
+    }
+
+    public function badRequestResponse($errors, $message = 'Bad Request'): JsonResponse
+    {
+        $data = [
+            'message' => $message,
+            'errors' => $errors,
+        ];
+        return new JsonResponse($data, 400);
+    }
+
+    public function unauthorizedResponse($errors = 'Unauthorized'): JsonResponse
+    {
+        $data = [
+            'message' => 'Failure',
+            'errors' => $errors,
+        ];
+
+        return new JsonResponse($data, 401);
+    }
+
+    public function forbiddenResponse($errors = 'Forbidden'): JsonResponse
+    {
+        $data = [
+            'message' => 'Failure',
+            'errors' => $errors,
+        ];
+
+        return new JsonResponse($data, 403);
+    }
+
+    public function notFoundResponse($errors = 'Not Found'): JsonResponse
+    {
+        $data = [
+            'message' => 'Failure',
+            'errors' => $errors,
+        ];
+
+        return new JsonResponse($data, 404);
+    }
+}
