3a4ff511f344bd6f8df799bdde56ca538fd4f824
StarrkF <faymergen98@gmail.com>
2023-04-08 00:55:57 +0300

    added post detail page & conf

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,14 @@ import './bootstrap';
 import './custom'
 import '@fortawesome/fontawesome-free/js/all.js';
 
-
 import { createApp } from "vue/dist/vue.esm-bundler.js"
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
 
 import Category from './pages/Category.vue'
 import Project from './pages/Project.vue'
-import SingleContent from './pages/SingleContent.vue'
+import SingleContent from './pages/SingleContent/datatable.vue'
 
 const app = createApp({})
 
@@ -15,5 +17,5 @@ const app = createApp({})
 app.component('category', Category)
 app.component('project', Project)
 app.component('single-content', SingleContent)
-
+app.component('QuillEditor', QuillEditor)
 app.mount('#app')
